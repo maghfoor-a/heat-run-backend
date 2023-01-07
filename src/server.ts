46c72d087a8c1ad0beb75538e248d537094ae9a4
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-07 15:49:02 +0000

    formatting

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import filePath from "./filePath";
+import { Client } from "pg";
 
 const app = express();
 
@@ -11,6 +12,10 @@ app.use(cors());
 dotenv.config();
 
 const PORT_NUMBER = process.env.PORT ?? 4000;
+
+//connecting to the elephantSQL database using URL
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
 
 app.get("/", (req, res) => {
   const pathToFile = filePath("../public/index.html");
